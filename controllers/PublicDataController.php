d9803e1a87e8730ce6c10e5aba626ef557c4d426
SemyonYa <mygarbage86@mail.ru>
2021-10-14 17:57:51 +0300

    public data in progress

--- controllers/PublicDataController.php
+++ controllers/PublicDataController.php
@@ -4,37 +4,39 @@ namespace app\controllers;
 
 use app\models\Ctor;
 use app\models\Image;
+use app\models\Place;
+use app\models\Project;
 
 class PublicDataController extends PublicRestController
 {
 
-    public function actionRegion($id)
-    {
-        return Ctor::find()->where(['tag' => 'region', 'id' => $id])->one();
-    }
+    // public function actionRegion($id)
+    // {
+    //     return Ctor::find()->where(['tag' => 'region', 'id' => $id])->one();
+    // }
 
-    public function actionPlaces($region_id)
+    public function actionPlaces($region)
     {
-        return Ctor::find()->where(['tag' => 'place', 'region_id' => $region_id])->all();
+        return Place::find()->where(['region' => $region])->all();
     }
 
     public function actionPlace($id)
     {
-        return Ctor::find()->where(['tag' => 'place', 'id' => $id])->one();
+        return Place::find()->where(['id' => $id])->one();
     }
 
-    public function actionObjects($place_id)
+    // public function actionObjects($place_id)
+    // {
+    //     return Ctor::find()->where(['tag' => 'object', 'place_id' => $place_id])->all();
+    // }
+
+    public function actionProject($id)
     {
-        return Ctor::find()->where(['tag' => 'object', 'place_id' => $place_id])->all();
+        return Project::find()->where(['id' => $id])->one();
     }
 
-    public function actionObject($id)
-    {
-        return Ctor::find()->where(['tag' => 'object', 'id' => $id])->one();
-    }
-
-    public function actionImages($object_id, $is_before = false)
-    {
-        return Image::find()->where(['object_id' => $object_id, 'is_before' => $is_before])->all();
-    }
+    // public function actionImages($object_id, $is_before = false)
+    // {
+    //     return Image::find()->where(['object_id' => $object_id, 'is_before' => $is_before])->all();
+    // }
 }
